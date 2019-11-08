31e8d486190306ffd644a8bf950d6da315c4046f
Santiago MVO <santiago.mvao@gmail.com>
2019-11-07 18:48:05 -0600

    added comments explaining every object and function in liri.js

--- liri.js
+++ liri.js
@@ -1,21 +1,28 @@
+//Retrieves API keys from hidden .env called in keys.js and assigns to keys variable
 require("dotenv").config();
 var keys = require("./keys.js");
 
+//Initialize packages
 var fs = require("fs");
 var axios = require("axios");
 var moment = require('moment');
 var inquirer = require("inquirer");
 var Spotify = require('node-spotify-api');
 
+//Receives command from 3rd argument and any other argument is taken as a string for calling APIs
 var command = process.argv[2];
 var args = process.argv.slice(3).join("+");
 
-
+//Object that contains OMDB API functionality
 var OMDB = {
+    //Query URL for OMDB with secret key
     queryUrl:`http://www.omdbapi.com/?apikey=${keys.OMDB.key}&y=&plot=short&t=`,
 
+    //Function to query and display in console a specific movie
     query:function (movie){
+        //If no movie argument is given, it defaults to "Inside Out"
         movie===""?movie="Inside+Out":"";
+        //Queries the server
         axios.get(this.queryUrl+movie).then(
             function(response) {
                 if(response.data.Response === "True"){
@@ -25,25 +32,35 @@ var OMDB = {
                     console.log(`The plot of the movie is: "${response.data.Plot}"`);
                     console.log(`Its main actors are ${response.data.Actors}.`);
                 }
+                //If there's a query error
                 else console.log(response.data.Error);
             })
+            //If there's a server error
             .catch(function(error) {
               console.log(error);
             });
     },
 }
 
+//Object that contains Bands in Town API functionality
 var BIT = {
+    //Query URL for Bands in Town, separated in two string so that the search paramenter can be insterted in between
     queryUrl:[`https://rest.bandsintown.com/artists/`,`/events?app_id=${keys.BIT.id}`],
 
+    //Funtion to query Bands in Town with given artist and display in console the response
     query:function (band){
+        //If no band argument is given, it defaults to "MGMT"
         band===""?band="MGMT":"";
+
+        //Queries Bands in Town
         axios.get(this.queryUrl[0]+band+this.queryUrl[1]).then(
             function(response) {
                 if(typeof response.data==="object"){
                     if(response.data.length===0){
+                        //If band is found but no concerts appear in the API response
                         console.log(`The band ${band} has no upcoming concerts.`);
                     }else{
+                        //Displays every venue given by API
                         for(event of response.data){
                             console.log(`Venue: ${event.venue.name} in ${event.venue.city}, ${event.venue.country}`);
                             console.log(moment(event.datetime).format("MM/DD/YYYY"));
@@ -51,6 +68,7 @@ var BIT = {
                         }
                     }
                 }else{
+                    //If band was not found or recognized by API
                     console.log("Band not found!");
                 }
             })
@@ -60,14 +78,20 @@ var BIT = {
     },
 }
 
+//Object that contains Spotify API functionality
 var SAPI = {
+    //Initialization of API object with API keys
     spotify:new Spotify({
         id: keys.spotify.id,
         secret:keys.spotify.secret
     }),
 
+    //Function to query server and display answer in console
     query:function(song){
+        //If no song argument is given, it defaults to "On Melancholy Hill" by Gorillaz
         song===""?song="on+melancholy+hill":"";
+
+        //Queries API with song given and returns only 1 song info
         SAPI.spotify.search({ type: 'track', query:song, limit:1}, function(err, data) {
             if (err) {
               return console.log('Error occurred: ' + err);
@@ -77,25 +101,29 @@ var SAPI = {
                 var artists = "";
                 var artistsArr = data.tracks.items[0].artists;
 
+                //Parses the response so that between them is a comma and before the last one is an "and"
                 for(let i = 0 ; i < artistsArr.length ; i++){
                     i===0?artists=artistsArr[i].name:i===artistsArr.length-1?artists+=" and "+artistsArr[i].name:artists+=", "+artistsArr[i].name;
                 }
 
-                
-                
                 console.log(`Artist${artistsArr.length===1?"":"s"}: ${artists}`);
                 console.log(`Song: ${data.tracks.items[0].name}`);
                 console.log(`Preview link: ${data.tracks.items[0].preview_url?data.tracks.items[0].preview_url:"None"}`);
                 console.log(`Album: ${data.tracks.items[0].album.name}`);
             } else {
+                //If song is not recognized or found by API
                 console.log("Song not found!");
             }
         })
     }
 }
 
+//Object that contains Inquirer package funtionality
 var Q = {
+    //Available commands
     commands:["movie-this","concert-this","spotify-this","exit"],
+
+    //Asks which command from list
     askCommand:function(){
         inquirer
         .prompt([
@@ -107,9 +135,12 @@ var Q = {
           }
         ])
         .then(function(response) {
+            //If user selects exit
             response.choice==="exit"?console.log("Thank you for using interactive mode..."):Q.askArg(response.choice);
         });
     },
+    
+    //Asks the arguments of the previous asked command
     askArg:function(command){
         inquirer
         .prompt([
@@ -135,25 +166,31 @@ var Q = {
     }
 }
 
+//Object that containg logging functioanlity using fs package
 var logger = {
+    //Logs command and arguments into log.txt, adding LIRI before them
     append:function(log){
         fs.appendFile("log.txt","LIRI "+log, function(err) {
             if (err) console.log(err);
         });
     },
+
+    //Displays in console everything in the file but the last comma
     read:function(){
         fs.readFile("log.txt", "utf8", function(err, data) {
             if (err) {
                 console.log(err);
             } else{
-                console.log(data);
+                console.log(data.substring(0,data.length-2));
             }
         });
     }
 }
 
+//Append every command
 logger.append(`${command}${args===""?"":" "+args}, `);
 
+//Switch to retrieve the command from terminal and send arguments to its respective object
 switch(command){
     case "movie-this":
         OMDB.query(args);
